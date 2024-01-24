3b963783eb15f137a0e8f7a89fc4c228aae0d703
VSKen <sayann.valmond@gmail.com>
2024-01-24 14:16:19 +0100

    Refactor print statement in 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
     if number < 99:
-        print(f"{number}, ", end="")
-    else:
+        print("{:02d}, ".format(number), end="")
+    else :
         print(number)

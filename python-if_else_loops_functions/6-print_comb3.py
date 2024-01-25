cc3fff7e475d75726536491fbe8a82c010424e8a
Vsayann <sayann.valmond@gmail.com>
2024-01-25 17:17:38 +0100

    Refactor print_comb3 function

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(90):
-    if number < 89:
-        if number % 10 > number / 10:
+    if number % 10 > number / 10:
+        if number != 89:
             print("{:02d}, ".format(number), end="")
-        else :
+        else:
             print("{:02d}".format(number))

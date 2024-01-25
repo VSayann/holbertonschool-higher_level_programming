c2ebe968101edc0b525be790fc9b157ab7d6a87e
Vsayann <sayann.valmond@gmail.com>
2024-01-25 17:23:35 +0100

    Refactor fizzbuzz function to use more descriptive variable names

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 15 == 0:
+    for number in range(1, 101):
+        if number % 5 == 0 and number % 3 == 0:
             print("FizzBuzz ", end="")
-        elif i % 5 == 0:
+        elif number % 5 == 0:
             print("Buzz ", end="")
-        elif i % 3 == 0:
+        elif number % 3 == 0:
             print("Fizz ", end="")
         else:
-            print("{} ".format(i), end="")
-    print("")
+            print("{} ".format(number), end="")

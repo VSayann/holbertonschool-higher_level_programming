0ab74f94169e963029e4d6802d397630c45efeef
VSKen <sayann.valmond@gmail.com>
2024-01-31 11:01:13 +0100

    Add multiple_returns function to return length and first character of a sentence

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if (sentence == ""):
+        sentence[0] == ""
+
+    length = len(sentence)
+    first = sentence[0]
+
+    return length, first
